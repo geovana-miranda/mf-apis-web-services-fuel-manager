df92794a6b0594a8b7cb4b42ed831078d73ae59b
Geovana Miranda <182geovana@gmail.com>
2025-08-08 14:05:51 -0300

    criação do Controller Usuario e criptografia da senha

--- mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
+++ mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
@@ -27,7 +27,7 @@ namespace mf_apis_web_services_fuel_manager.Controllers
         public async Task<ActionResult> Create(Veiculo model)
         {
 
-            if(model.AnoFabricacao <= 0 || model.AnoModelo <= 0)
+            if (model.AnoFabricacao <= 0 || model.AnoModelo <= 0)
             {
                 return BadRequest(new { message = "Ano de Fabricação e Ano Modelo devem ser maiores que zero" });
             }
@@ -41,10 +41,11 @@ namespace mf_apis_web_services_fuel_manager.Controllers
         public async Task<ActionResult> GetById(int id)
         {
             var model = await _context.Veiculos
+                .Include(t => t.Usuarios).ThenInclude(t => t.Usuario)
                 .Include(t => t.Consumos)
                 .FirstOrDefaultAsync(c => c.Id == id);
 
-            if(model == null)
+            if (model == null)
             {
                 return NotFound();
             }
@@ -87,5 +88,36 @@ namespace mf_apis_web_services_fuel_manager.Controllers
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "update", metodo: "PUT"));
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "delete", metodo: "Delete"));
         }
+
+        [HttpPost("{id}/usuarios")]
+        public async Task<ActionResult> AddUsuario(int id, VeiculoUsuario model)
+        {
+            if (id != model.VeiculoId) return BadRequest();
+
+            _context.VeiculoUsuarios.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new { id = model.VeiculoId }, model);
+        }
+
+        [HttpDelete("{id}/usuarios/{usuarioId}")]
+        public async Task<ActionResult> DeleteUsuario(int id, int usuarioId)
+        {
+            var model = await _context.VeiculoUsuarios
+                .Where(c => c.VeiculoId == id && c.UsuarioId == usuarioId)
+                .FirstOrDefaultAsync();
+
+            if (model == null) return NotFound();
+
+            _context.VeiculoUsuarios.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+           
+        }
+
+
     }
+
+
 }
